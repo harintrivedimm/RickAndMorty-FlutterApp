fc6f57fc99d1f5dd12e524f885c39ec11e38f3c9
Harin Trivedi <harin.trivedi@mutualmobile.com>
2023-10-10 15:24:45 +0530

    Onboarding screen design

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class RickAndMortyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Rick & Morty',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFE6E0E9)),
         useMaterial3: true,
       ),
       home: const HomePage(title: 'Rick & Morty'),
@@ -31,19 +31,93 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final PageController controller = PageController();
+  final int total = 3;
+  int page = 1;
+
+  void _nextPage() {
+    page++;
+    if(page <= total) {
+      controller.nextPage(duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+    }
+    else if(page > total)
+    {
+      _navigateMain();
+    }
+  }
+
+  void _navigateMain() {
+    //TODO
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(12),
+      child : Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Image(image: AssetImage('assets/banner.png'), height: 100),
+          Expanded(
+            flex: 1,
+            child: PageView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: controller,
+              children: const <Widget>[
+                Banner(image: "assets/image_1.png", title: "Get Schwifty", description: "Get ready to have some \nfun with Rick and Morty"),
+                Banner(image: "assets/image_2.png", title: "Wubba Lubba Dub Dub", description: "Welcome to the Rick \nand Morty universe"),
+                Banner(image: "assets/image_3.png", title: "To Infinity and Beyond", description: "Explore the multiverse \nwith Rick and Morty"),
+              ],
+            ),
+          ),
+          Container(
+              margin: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                      onPressed: () { _navigateMain(); },
+                      child: const Text("Skip", style: TextStyle(decoration: TextDecoration.none, color: Colors.black, fontSize: 20))
+                  ),
+                  FloatingActionButton(
+                    onPressed: _nextPage,
+                    tooltip: 'Next',
+                    child: const Icon(Icons.arrow_forward),
+                  )
+                ],
+              )
+          )
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'Rick & Morty',
-        ),
-      ),
+    );
+  }
+
+}
+
+class Banner extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  const Banner({
+    super.key, required this.image, required this.title, required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(image: AssetImage(image), alignment: Alignment.center, fit: BoxFit.fitHeight),
+          const Padding(padding: EdgeInsets.all(10)),
+          Text(title, textAlign: TextAlign.center,
+              style: const TextStyle(decoration: TextDecoration.none, color: Colors.black, fontStyle: FontStyle.normal, fontSize: 30)),
+          const Padding(padding: EdgeInsets.all(10)),
+          Text(description, textAlign: TextAlign.center,
+              style: const TextStyle(decoration: TextDecoration.none, color: Colors.black, fontStyle: FontStyle.normal, fontSize: 18)),
+        ]
     );
   }
 }
